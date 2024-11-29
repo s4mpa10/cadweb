4f880fc0bd17bbbe49b41ab8f1e057c7def1ae7e
s4mpa10 <universitario.alan777@gmail.com>
2024-11-29 07:24:56 -0300

    V3

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 import locale
 from django.db import models
 
+class Categoria(models.Model): 
+    nome = models.CharField(max_length = 100)
+    ordem = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
