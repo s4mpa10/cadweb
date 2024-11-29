e62855d3328def48018f2c047a008faa2a3041e5
s4mpa10 <universitario.alan777@gmail.com>
2024-11-29 17:16:02 -0300

    v1 - Atividade back e front

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
-
+from .models import Categoria
 
 def index(request):
     return render(request,'index.html')
 
+def categoria(request): 
+    contexto = {
+        'lista': Categoria.objects.all().order_by('id'),
+    }
+    return render(request, 'categoria/lista.html', contexto)
