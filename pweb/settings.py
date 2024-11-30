8b1df1a111fb6391b9f5b102bd0766c7debcb75f
s4mpa10 <universitario.alan777@gmail.com>
2024-11-30 13:59:14 -0300

    v1 - Atividade

--- pweb/settings.py
+++ pweb/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': dj_database_url.parse('postgres://neondb_owner:xgtpikmY3bA7@ep-super-cherry-a4ljnzil-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require') 
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
