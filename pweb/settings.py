c01396aa28c2877ed3776e0513124273c43888fd
s4mpa10 <universitario.alan777@gmail.com>
2025-02-09 22:44:05 -0300

    Erro no @login_required

--- pweb/settings.py
+++ pweb/settings.py
@@ -137,3 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
